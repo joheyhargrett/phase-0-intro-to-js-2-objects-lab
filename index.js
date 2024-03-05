f69978a92355f96ea579d9ee52bb3e7e87ff0031
Johey Hargrett <joheyhargrett@gmail.com>
2024-03-05 15:02:39 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+let employee = {};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  // Non-destructive update
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  // Destructive update
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  // Non-destructive delete
+  let newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  // Destructive delete
+  delete employee[key];
+  return employee;
+}
